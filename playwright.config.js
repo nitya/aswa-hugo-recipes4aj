4b99257c1c9ccbb325a0afc91a5ea8cbea4d6e56
Nitya Narasimhan <n.narasimhan@gmail.com>
2021-12-06 16:57:31 -0500

    Resetting actions ...

--- playwright.config.js
+++ playwright.config.js
@@ -7,7 +7,7 @@ const path = require('path');
  * @type {import('@playwright/test').PlaywrightTestConfig}
  */
 const config = {
-  testDir: './tests',
+  testDir: './e2e',
 
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
