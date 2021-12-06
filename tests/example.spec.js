cf6f3c8356b966ba561d1b7b829b06a018a57b96
Nitya Narasimhan <n.narasimhan@gmail.com>
2021-12-06 14:10:55 -0500

    Adding Playwright e2e test integration

---
+++ tests/example.spec.js
@@ -0,0 +1,9 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('basic test', async ({ page }) => {
+  await page.goto('https://bit.ly/recipes-for-aj');
+  await expect(page).toHaveTitle("Recipes 4 AJ");
+
+  await page.locator('text=View Recipe').click();
+});
